e2681d31141a45da453cb2ba9f75679a9f8c8247
Alexander Popp <alexpopp.1991@gmail.com>
2015-04-18 10:26:08 +0200

    Basic register UI

--- client/src/main/java/crowdtrip/com/croudtrip/activities/LoginActivity.java
+++ client/src/main/java/crowdtrip/com/croudtrip/activities/LoginActivity.java
@@ -1,10 +1,14 @@
 package crowdtrip.com.croudtrip.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import org.crowdtrip.HelloWorld;
@@ -26,19 +30,75 @@ import rx.schedulers.Schedulers;
  */
 public class LoginActivity extends Activity {
 
+    private View layoutRegister, layoutChoose;
+    private int animationDuration;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        animationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-        final EditText serverInput = (EditText) findViewById(R.id.input_server);
+        layoutRegister = findViewById(R.id.layout_register);
+        layoutChoose = findViewById(R.id.layout_choose);
+
+        layoutRegister.setVisibility(View.GONE);
+
+
+        Button loginEmail = (Button) findViewById(R.id.btn_login_email);
+        loginEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterView();
+            }
+        });
+
+        final EditText firstName = (EditText) findViewById(R.id.et_firstName);
+        final EditText lastName = (EditText) findViewById(R.id.et_lastName);
+        final EditText email = (EditText) findViewById(R.id.et_email);
+
+        Button register = (Button) findViewById(R.id.btn_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+
+        /*final EditText serverInput = (EditText) findViewById(R.id.input_server);
         findViewById(R.id.hello_world).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getHelloWorld(serverInput.getText().toString());
             }
-        });
+        });*/
+    }
+
+    private void showRegisterView() {
+        layoutRegister.setAlpha(0f);
+        layoutRegister.setVisibility(View.VISIBLE);
+
+        layoutRegister.setTranslationY(-100);
+        layoutRegister.animate()
+                .setStartDelay(animationDuration/2)
+                .alpha(1f)
+                .translationY(100)
+                .setDuration(animationDuration)
+                .setListener(null);
+
+        layoutChoose.animate()
+                .alpha(0f)
+                .translationY(200)
+                .setDuration(animationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        layoutChoose.setVisibility(View.GONE);
+                    }
+                });
     }
 
 
