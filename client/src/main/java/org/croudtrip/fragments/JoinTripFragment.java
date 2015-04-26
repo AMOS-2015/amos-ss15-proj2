65ce3ddcb70d1c98f8ca1bedf2f62fa3b573d098
Alexander Popp <alexpopp.1991@gmail.com>
2015-04-26 13:45:51 +0200

    client: Added places api with basic place picker

--- client/src/main/java/org/croudtrip/fragments/JoinTripFragment.java
+++ client/src/main/java/org/croudtrip/fragments/JoinTripFragment.java
@@ -1,15 +1,24 @@
 package org.croudtrip.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import org.croudtrip.R;
 
@@ -20,6 +29,11 @@ import it.neokree.materialnavigationdrawer.elements.MaterialSection;
  * Created by alex on 22.04.15.
  */
 public class JoinTripFragment extends Fragment {
+
+    private final int REQUEST_PLACE_PICKER = 122;
+
+    private TextView tv_name, tv_address, tv_attributions;
+    private Button btn_destination;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +48,31 @@ public class JoinTripFragment extends Fragment {
 
         setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_join_trip, container, false);
+
+        tv_name = (TextView) view.findViewById(R.id.name);
+        tv_address = (TextView) view.findViewById(R.id.address);
+        tv_attributions = (TextView) view.findViewById(R.id.attributions);
+
+
+
+        btn_destination = (Button) view.findViewById(R.id.places);
+        btn_destination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+                    Intent intent = intentBuilder.build(getActivity().getApplicationContext());
+
+                    startActivityForResult(intent, REQUEST_PLACE_PICKER);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                   e.printStackTrace();
+                }
+            }
+        });
+
+
 
         FloatingActionButton btn_offer = (FloatingActionButton) view.findViewById(R.id.btn_offer_trip);
         final Fragment _this = this;
@@ -54,6 +93,29 @@ public class JoinTripFragment extends Fragment {
 
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PLACE_PICKER && resultCode == Activity.RESULT_OK) {
+
+            // The user has selected a place. Extract the tv_name and tv_address.
+            final Place place = PlacePicker.getPlace(data, getActivity());
+
+            final CharSequence name = place.getName();
+            final CharSequence address = place.getAddress();
+            String attributions = PlacePicker.getAttributions(data);
+            if (attributions == null) {
+                attributions = "";
+            }
+
+            tv_name.setText(name);
+            tv_address.setText(address);
+            tv_attributions.setText(Html.fromHtml(attributions));
+            btn_destination.setText(getResources().getString(R.string.join_change_destination));
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
