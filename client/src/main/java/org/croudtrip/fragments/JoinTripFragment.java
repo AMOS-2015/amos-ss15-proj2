91b3d7e45cfb5ea44ce8f809627734a9c51511aa
Frederik Simon <frederik.simon@fau.de>
2015-04-30 18:26:13 +0200

    client: added join trip request from client

--- client/src/main/java/org/croudtrip/fragments/JoinTripFragment.java
+++ client/src/main/java/org/croudtrip/fragments/JoinTripFragment.java
@@ -5,6 +5,9 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
@@ -23,15 +26,30 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 import org.croudtrip.Constants;
 import org.croudtrip.R;
+import org.croudtrip.TripsResource;
+import org.croudtrip.directions.RouteLocation;
+import org.croudtrip.location.LocationUpdater;
+import org.croudtrip.trips.TripMatch;
+import org.croudtrip.trips.TripRequestDescription;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
 import roboguice.fragment.provided.RoboFragment;
 import roboguice.inject.InjectView;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 /**
  * Created by alex on 22.04.15.
@@ -45,9 +63,15 @@ public class JoinTripFragment extends RoboFragment {
     @InjectView(R.id.address) private EditText tv_address;
     @InjectView(R.id.places) private Button btn_destination;
 
+    @Inject LocationUpdater locationUpdater;
+    @Inject TripsResource tripsResource;
+    private Geocoder geocoder;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        geocoder = new Geocoder(getActivity());
 
         Toolbar toolbar = ((MaterialNavigationDrawer) this.getActivity()).getToolbar();
     }
@@ -56,7 +80,7 @@ public class JoinTripFragment extends RoboFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        super.onCreateView( inflater, container, savedInstanceState );
+        super.onCreateView(inflater, container, savedInstanceState);
         setHasOptionsMenu(true);
 
         View view = inflater.inflate(R.layout.fragment_join_trip, container, false);
@@ -97,6 +121,56 @@ public class JoinTripFragment extends RoboFragment {
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putInt(Constants.SHARED_PREF_KEY_WAITING_TIME, Integer.valueOf(maxWaitingTime.getText().toString()));
                 editor.apply();
+
+                // retrieve current position
+                Location currentLocation = locationUpdater.getLastLocation();
+                if( currentLocation == null ) {
+                    Toast.makeText(getActivity().getApplicationContext(), R.string.offer_trip_no_location, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // get destination from string
+                LatLng destination = null;
+                try {
+                    List<Address> addresses = geocoder.getFromLocationName( tv_address.getText().toString(), 1 );
+                    if( addresses.size() > 0 )
+                        destination = new LatLng( addresses.get(0).getLatitude(), addresses.get(0).getLongitude() );
+                } catch (IOException e) {
+                    Toast.makeText(getActivity().getApplicationContext(), R.string.offer_trip_no_destination, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // no destination received
+                if( destination == null ) {
+                    Toast.makeText(getActivity().getApplicationContext(), R.string.offer_trip_no_destination, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                TripRequestDescription tripRequestDescription = new TripRequestDescription( new RouteLocation( currentLocation.getLatitude(), currentLocation.getLongitude() ),
+                                                                                            new RouteLocation( destination.latitude, destination.longitude ));
+
+                tripsResource.findMatches( tripRequestDescription ).subscribeOn(Schedulers.io())
+                                                                   .observeOn(AndroidSchedulers.mainThread())
+                                                                   .subscribe( new Action1<List<TripMatch>>() {
+                                                                       @Override
+                                                                       public void call(List<TripMatch> tripMatches) {
+
+                                                                           if( tripMatches == null || tripMatches.size() == 0 ) {
+                                                                                Toast.makeText(getActivity().getApplicationContext(), R.string.join_trip_no_matches, Toast.LENGTH_SHORT).show();
+                                                                                return;
+                                                                           }
+                                                                           Toast.makeText(getActivity().getApplicationContext(), "Found " + tripMatches.size() + " matches", Toast.LENGTH_SHORT).show();
+                                                                           TripMatch tripMatch = tripMatches.get(0);
+
+                                                                       }
+                                                                   }, new Action1<Throwable>() {
+                                                                       @Override
+                                                                       public void call(Throwable throwable) {
+                                                                           // on main thread; something went wrong
+                                                                           Timber.e(throwable.getMessage());
+                                                                       }
+                                                                   });
+
                 Toast.makeText(getActivity().getApplicationContext(), "Join Trip", Toast.LENGTH_SHORT).show();
             }
         });
@@ -107,7 +181,7 @@ public class JoinTripFragment extends RoboFragment {
         btn_offer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MaterialNavigationDrawer drawer = (MaterialNavigationDrawer)_this.getActivity();
+                MaterialNavigationDrawer drawer = (MaterialNavigationDrawer) _this.getActivity();
                 drawer.setFragment(OfferTripFragment.get(), getString(R.string.menu_offer_trip));
                 MaterialSection section = drawer.getSectionByTitle(getString(R.string.menu_offer_trip));
                 drawer.setSection(section);
@@ -118,6 +192,7 @@ public class JoinTripFragment extends RoboFragment {
                  */
             }
         });
+
     }
 
     @Override
