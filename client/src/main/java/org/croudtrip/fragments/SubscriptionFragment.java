8718659700d7d1c12636132ae4205dccf6e34f33
Frederik Simon <frederik.simon@fau.de>
2015-05-07 09:56:03 +0200

    client: added subscription fragment that you can extend to make sure that your subscriptions added manually to the composite subscription are unsubscribed on context change

---
+++ client/src/main/java/org/croudtrip/fragments/SubscriptionFragment.java
@@ -0,0 +1,31 @@
+package org.croudtrip.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.croudtrip.R;
+
+import roboguice.fragment.provided.RoboFragment;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * A base class fragments that extends the {@link RoboFragment} to provide dependency injection
+ * and that handles a {@link rx.subscriptions.CompositeSubscription} that can be used in subclasses
+ * to unsubscribe all the subscription if the lifecycle of the fragment ends.
+ */
+public class SubscriptionFragment extends RoboFragment {
+    protected final CompositeSubscription subscriptions = new CompositeSubscription();
+
+    @Override
+    public void onStop() {
+        subscriptions.unsubscribe();
+        subscriptions.clear();
+    }
+
+
+}
