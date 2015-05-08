eb9192917e3338c6ccaf7729c5d1118fe634d474
Frederik Simon <frederik.simon@fau.de>
2015-05-09 00:36:56 +0200

    client: prepared notification to open an intent - still right action has to be done after starting the activity

--- client/src/main/java/org/croudtrip/gcm/GcmIntentService.java
+++ client/src/main/java/org/croudtrip/gcm/GcmIntentService.java
@@ -9,10 +9,13 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import org.croudtrip.R;
 import org.croudtrip.account.AccountManager;
+import org.croudtrip.activities.MainActivity;
 import org.croudtrip.api.TripsResource;
 import org.croudtrip.api.gcm.GcmConstants;
 import org.croudtrip.api.trips.JoinTripRequest;
@@ -42,18 +45,6 @@ public class GcmIntentService extends IntentService {
 
         final Context context = getApplicationContext();
 
-        // create rest request handler
-        TripsResource tripsResource = new RestAdapter.Builder()
-                .setEndpoint(getString(R.string.server_address))
-                .setRequestInterceptor(new RequestInterceptor() {
-                    @Override
-                    public void intercept(RequestFacade request) {
-                        AccountManager.addAuthorizationHeader(context, request);
-                    }
-                })
-                .build()
-                .create(TripsResource.class);
-
         // check for proper GCM message
         String messageType = GoogleCloudMessaging.getInstance(context).getMessageType(intent);
         if (!GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) return;
@@ -67,50 +58,7 @@ public class GcmIntentService extends IntentService {
             case GcmConstants.GCM_MSG_DUMMY:
                 break;
             case GcmConstants.GCM_MSG_JOIN_REQUEST:
-                Timber.d("JOIN_REQUEST");
-
-                // extract join request and offer from message
-                long joinTripRequestId = Long.parseLong( intent.getExtras().getString(GcmConstants.GCM_MSG_JOIN_REQUEST_ID) );
-                long offerId = Long.parseLong( intent.getExtras().getString(GcmConstants.GCM_MSG_JOIN_REQUEST_OFFER_ID) );
-
-                // download the join trip request
-                tripsResource.getJoinRequest(offerId, joinTripRequestId ).observeOn( Schedulers.io() )
-                                        .subscribeOn( AndroidSchedulers.mainThread() )
-                                        .subscribe(
-                                                new Action1<JoinTripRequest>() {
-                                                   @Override
-                                                   public void call(JoinTripRequest joinTripRequest) {
-                                                       // create notification for the user
-                                                       // TODO: Send the join trip request or at least the join trip request id
-                                                       if(LifecycleHandler.isApplicationInForeground()) {
-                                                           // TODO: start request activity immediately
-                                                       } else {
-                                                           createNotification(getString(R.string.join_request_title), getString(R.string.joint_request_msg, joinTripRequest.getQuery().getPassenger().getFirstName()), GcmConstants.GCM_NOTIFICATION_JOIN_REQUEST_ID);
-                                                       }
-                                                   }
-                                                },
-                                                new Action1<Throwable>() {
-                                                    @Override
-                                                    public void call(Throwable throwable) {
-                                                        Timber.e("Something went wrong when downloading join request: " + throwable.getMessage());
-                                                    }
-                                                });
-
-                // TODO: This is just for testing. Will be done cleanly and combined with the UI and android notifications.
-                // always accepting for testing
-                JoinTripRequestUpdate requestUpdate = new JoinTripRequestUpdate( true );
-                tripsResource.updateJoinRequest( offerId, joinTripRequestId, requestUpdate ).observeOn(Schedulers.io()).subscribeOn(Schedulers.newThread()).subscribe(new Action1<JoinTripRequest>() {
-                    @Override
-                    public void call(JoinTripRequest joinTripRequest) {
-
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        // on main thread; something went wrong
-                        Timber.e("Error when trying to join a trip: " + throwable.getMessage());
-                    }
-                });
+                handleJoinRequest(intent);
 
                 break;
             default:
@@ -123,17 +71,92 @@ public class GcmIntentService extends IntentService {
         GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
 
-    private void createNotification( String title, String message, int notificationId ) {
-        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+    private void handleJoinRequest(Intent intent) {
+        Timber.d("JOIN_REQUEST");
 
-        //PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, .class), 0);
+        final ObjectMapper mapper = new ObjectMapper();
+
+        // create rest request handler
+        TripsResource tripsResource = new RestAdapter.Builder()
+                .setEndpoint(getString(R.string.server_address))
+                .setRequestInterceptor(new RequestInterceptor() {
+                    @Override
+                    public void intercept(RequestFacade request) {
+                        AccountManager.addAuthorizationHeader(getApplicationContext(), request);
+                    }
+                })
+                .build()
+                .create(TripsResource.class);
+
+        // extract join request and offer from message
+        long joinTripRequestId = Long.parseLong( intent.getExtras().getString(GcmConstants.GCM_MSG_JOIN_REQUEST_ID) );
+        long offerId = Long.parseLong( intent.getExtras().getString(GcmConstants.GCM_MSG_JOIN_REQUEST_OFFER_ID) );
+
+        // download the join trip request
+        tripsResource.getJoinRequest(offerId, joinTripRequestId ).observeOn( Schedulers.io() )
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        new Action1<JoinTripRequest>() {
+                            @Override
+                            public void call(JoinTripRequest joinTripRequest) {
+                                // create notification for the user
+                                // TODO: Send the join trip request or at least the join trip request id
+                                if (LifecycleHandler.isApplicationInForeground()) {
+                                    // TODO: start request activity immediately
+                                } else {
+                                    Intent startingIntent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startingIntent.setAction("HAVE_TO_INSERT_SOME_ACTION_STRING");
+                                    try {
+                                        startingIntent.putExtra("ANOTHER_STRING_THAT_CURRENTLY_NOT_EXISTS", mapper.writeValueAsString(joinTripRequest));
+                                    } catch (JsonProcessingException e) {
+                                        Timber.e(e.getMessage());
+                                    }
+                                    PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, startingIntent, 0);
+                                    createNotification(getString(R.string.join_request_title), getString(R.string.joint_request_msg,
+                                                    joinTripRequest.getQuery().getPassenger().getFirstName()),
+                                            GcmConstants.GCM_NOTIFICATION_JOIN_REQUEST_ID, contentIntent);
+                                }
+                            }
+                        },
+                        new Action1<Throwable>() {
+                            @Override
+                            public void call(Throwable throwable) {
+                                Timber.e("Something went wrong when downloading join request: " + throwable.getMessage());
+                            }
+                        });
+
+
+        // TODO: This is just for testing. Will be done cleanly and combined with the UI and android notifications.
+        // always accepting for testing
+        JoinTripRequestUpdate requestUpdate = new JoinTripRequestUpdate( true );
+        tripsResource.updateJoinRequest( offerId, joinTripRequestId, requestUpdate ).observeOn(Schedulers.io()).subscribeOn(Schedulers.newThread()).subscribe(new Action1<JoinTripRequest>() {
+            @Override
+            public void call(JoinTripRequest joinTripRequest) {
+
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                // on main thread; something went wrong
+                Timber.e("Error when trying to join a trip: " + throwable.getMessage());
+            }
+        });
+    }
+
+    private void createNotification( String title, String message, int notificationId ) {
+        createNotification(title, message, notificationId, null);
+    }
+
+    private void createNotification( String title, String message, int notificationId, PendingIntent contentIntent ) {
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         Notification notification  =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_directions_car_white)
-                        .setContentTitle( title )
+                        .setContentTitle(title)
                         .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
                         .setContentText(message)
+                        .setContentIntent(contentIntent)
                         .build();
 
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
