14de3aeff5695b980ff1358eaab9f941ec4166e4
Vanessa Lange <vanessa.lange@fau.de>
2015-06-07 20:07:16 +0200

    Client/Server: Test for user registration with valid user input

--- client/src/androidTest/java/org/croudtrip/LoginTests.java
+++ client/src/androidTest/java/org/croudtrip/LoginTests.java
@@ -1,18 +1,22 @@
 package org.croudtrip;
 
-import static android.support.test.espresso.Espresso.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
-
-
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
 import android.test.ActivityInstrumentationTestCase2;
-
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.croudtrip.account.AccountManager;
 import org.croudtrip.activities.LoginActivity;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by alex on 06.06.15.
@@ -39,12 +43,66 @@ public class LoginTests extends ActivityInstrumentationTestCase2<LoginActivity> 
         loginActivity = getActivity();
     }
 
-    public void testCreateAccount() {
-        onView(withId(R.id.btn_register_email)).perform(click());
-        //onView(withId(R.id.btn_register)).perform(click());
+    public void testCreateValidAccount() throws InterruptedException{
 
-        onView(withId(R.id.btn_register))
-                .check(matches(withText(R.string.register_button)));
+        // Show correct register view after clicking "Register"
+        onView(withId(R.id.btn_register_email)).perform(click());
+        onView(withId(R.id.btn_register)).check(matches(withText(R.string.register_button)));
+
+        // Register a new random user
+        final String ABC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String firstName = generateRandomString(20, ABC, "");
+        String lastName = generateRandomString(20, ABC, "");
+        String email = generateRandomString(20, ABC, "@.");
+        String password = generateRandomString(20, ABC, "");
+
+        // Use closeSoftKeyboard and Thread.sleep because Espresso is buggy
+        // and crashes if the EditText is not visible due to the keyboard (and it doesn't close
+        // fast enough -> sleep necessary :( )
+        onView(withId(R.id.et_email)).perform(typeText(email));
+        closeSoftKeyboard(); Thread.sleep(1000);
+
+        onView(withId(R.id.et_firstName)).perform(typeText(firstName));
+        closeSoftKeyboard(); Thread.sleep(1000);
+
+        onView(withId(R.id.et_lastName)).perform(typeText(lastName));
+        closeSoftKeyboard(); Thread.sleep(1000);
+
+        onView(withId(R.id.et_password)).perform(typeText(password));
+        closeSoftKeyboard(); Thread.sleep(1000);
+
+        onView(withId(R.id.btn_register)).perform(click());
+        assertTrue(AccountManager.isUserLoggedIn(loginActivity));
     }
 
+
+    private String generateRandomString(int length, String allowedChars, String mustHaveChars){
+
+        if(mustHaveChars.length() > length){
+            return null;
+        }
+
+        // Build a random string from the allowed chars
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(allowedChars.charAt(random.nextInt(allowedChars.length())));
+        }
+
+        // Insert must-have characters at different positions in the string
+        ArrayList<Integer> mustHaveCharsIndices = new ArrayList<Integer>();
+        for(int i = 0; i < mustHaveChars.length(); i++){
+            char mustHave = mustHaveChars.charAt(i);
+
+            int index = -1;
+            do{
+                index = random.nextInt(length);
+            }while(mustHaveCharsIndices.contains(index));
+
+            mustHaveCharsIndices.add(index);
+            builder.setCharAt(index, mustHave);
+        }
+
+        return builder.toString();
+    }
 }
